0f052ad6d0f5ef77c48b75e7b88584645579002d
Ramil Novruzov <ramikoff@gmail.com>
2021-08-18 09:50:58 +0200

    Updates

---
+++ precache-manifest.783a1ef106002bc88e0bc5b455130d21.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d7fa450811395937d4d7270b510675a",
+    "url": "/react-keeper-app/index.html"
+  },
+  {
+    "revision": "728ccff01a6fdb40ffb4",
+    "url": "/react-keeper-app/static/js/2.f1932649.chunk.js"
+  },
+  {
+    "revision": "3680e6b1cadd228331b6",
+    "url": "/react-keeper-app/static/js/main.8a98a96c.chunk.js"
+  },
+  {
+    "revision": "a4159de13dff08f81c7d",
+    "url": "/react-keeper-app/static/js/runtime-main.beffe921.js"
+  }
+]);
